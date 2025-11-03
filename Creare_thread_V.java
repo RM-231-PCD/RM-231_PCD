dd80e1defaa8a3ff26ead5c0446d25af468386c8
Stony21 <viktoriamuntan28@gmail.com>
2025-11-03 10:49:52 +0200

    cod redactat

---
+++ Creare_thread_V.java
@@ -0,0 +1,59 @@
+public class Creare_thread_V {
+    private String memberName;
+    private int[] sharedArray;
+
+    public Creare_thread_V (String name,int[]array){
+        this.memberName=name;
+        this.sharedArray=array;
+    }
+
+    class Th1 extends Thread{
+        @Override
+        public void run(){
+            System.out.println(memberName +" Th1 Definitia produselor de la inceput");
+         
+            int diff=0;
+            for (int i=0;i<sharedArray.length-2;i+=2){
+                int prod1 = sharedArray[i] * sharedArray[i + 1];       
+                int prod2 = sharedArray[i + 2] * sharedArray[i + 3 < sharedArray.length ? i + 3 : i + 2];
+                diff += Math.abs(prod1 - prod2);  
+            }   
+
+            System.out.println(memberName + " Diferenta totala (de la inceput): " + diff);
+        }
+    }
+
+    class Th2 extends Thread{
+        @Override
+        public void run(){
+            System.out.println(memberName +" Th2 Definitia produselor de la sfarsit");
+            
+        int diff = 0;
+        for (int i = sharedArray.length - 1; i >= 3; i -= 2) { 
+            int prod1 = sharedArray[i] * sharedArray[i - 1];
+            int prod2 = sharedArray[i - 2] * sharedArray[i - 3];
+            diff += Math.abs(prod1 - prod2);
+        }
+
+        System.out.println(memberName + " Diferenta totala (de la sfarsit): " + diff);
+    }
+}
+
+    public void startThreads(){
+        Th1 t1=new Th1();
+        Th2 t2=new Th2();
+
+        System.out.println(memberName +" Starting threads...");
+        t1.start();
+        t2.start();
+        try{
+            t1.join();
+            t2.join();
+            System.out.println(memberName +" Threads finished.");
+        }
+        catch(InterruptedException e){
+            System.out.println(memberName +" Thread interrupted: " + e.getMessage());
+        }
+    }
+}
+    
