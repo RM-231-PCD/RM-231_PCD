63416c6d7da3ec9684b1c994a381ab743971da23
catalinprepelita39-ux <catalinprepelita39@gmail.com>
2025-11-24 10:30:25 +0200

    save before pool

--- lab1.java
+++ lab1.java
@@ -1,9 +1,11 @@
-public class Lab1 {
-    public static void main(String []args){
+
+public class lab1 {
+
+    public static void main(String[] args) {
         int tablou[] = new int[100];
 
-        for(int i=0; i<100; i++){
-            tablou[i] = (int)(Math.random()*100);
+        for (int i = 0; i < 100; i++) {
+            tablou[i] = (int) (Math.random() * 100);
 
             System.out.print(" " + tablou[i]);
         }
@@ -16,7 +18,7 @@ public class Lab1 {
         t1.setName("fir_1");
         t1.start();
 
-        Thread_2 r2 = new Thread_2(tablou,  tablou.length -1, -1, -1);
+        Thread_2 r2 = new Thread_2(tablou, tablou.length - 1, -1, -1);
         Thread t2 = new Thread(r2);
         t2.setName("fir_2");
         t2.start();
@@ -25,21 +27,18 @@ public class Lab1 {
         Thread t3 = new Thread(r3);
         t3.setName("fir_3");
         t3.start();
-       
-
 
         Thread_4 r4 = new Thread_4(tablou, tablou.length - 1, -1, -2);
         Thread t4 = new Thread(r4);
         t4.setName("fir_4");
         t4.start();
 
-
     }
 }
 
-
 class Thread_1 implements Runnable {
-    int tablou[]; 
+
+    int tablou[];
     int from;
     int to;
     int step;
@@ -59,17 +58,16 @@ class Thread_1 implements Runnable {
         int c = 0;
         int s = 0;
 
-
         for (int i = from; i != to; i += step) {
-            if (tablou[i] %2 != 0) {
+            if (tablou[i] % 2 != 0) {
                 c++;
                 if (c <= 2) {
                     p = p * tablou[i];
-                } else if (c <=4){
+                } else if (c <= 4) {
                     d = d * tablou[i];
                     s = p + d;
                 }
-                if ( c == 4) {
+                if (c == 4) {
                     System.out.println("Produsul este: " + Thread.currentThread().getName() + " " + "primul produs: " + p + " al doilea produs: " + d + " suma: " + s);
                     p = 1;
                     d = 1;
@@ -86,41 +84,40 @@ class Thread_1 implements Runnable {
     }
 }
 
-    class Thread_2 implements Runnable {
-        int tablou[];
-        int from;
-        int to;
-        int step;
+class Thread_2 implements Runnable {
 
-        public Thread_2(int tablou[], int from, int to, int step) {
-            this.tablou = tablou;
-            this.from = from;
-            this.to = to;
-            this.step = step;
-        }
+    int tablou[];
+    int from;
+    int to;
+    int step;
 
+    public Thread_2(int tablou[], int from, int to, int step) {
+        this.tablou = tablou;
+        this.from = from;
+        this.to = to;
+        this.step = step;
+    }
 
-        @Override
-        public void run() {
+    @Override
+    public void run() {
 
-            int p = 1;
-            int c = 0;
-            int d = 1;
-            int s = 0;
+        int p = 1;
+        int c = 0;
+        int d = 1;
+        int s = 0;
 
-            for (int i = from; i > to; i += step) {
-                
+        for (int i = from; i > to; i += step) {
 
-            if (tablou[i] %2 != 0) {
+            if (tablou[i] % 2 != 0) {
                 c++;
                 if (c <= 2) {
                     p = p * tablou[i];
-                } else if (c <=4){
+                } else if (c <= 4) {
                     d = d * tablou[i];
                     s = p + d;
                 }
-                if ( c == 4) {
-                   System.out.println("Produsul este: " + Thread.currentThread().getName() + " " + "primul produs: " + p + " al doilea produs: " + d + " suma: " + s);
+                if (c == 4) {
+                    System.out.println("Produsul este: " + Thread.currentThread().getName() + " " + "primul produs: " + p + " al doilea produs: " + d + " suma: " + s);
                     p = 1;
                     d = 1;
                     c = 0;
@@ -128,10 +125,7 @@ class Thread_1 implements Runnable {
                 }
             }
         }
-         
-        
-        
-        
+
         String numePrenume = "Dodon Cristin";
         for (char simbol : numePrenume.toCharArray()) {
             System.out.print(simbol);
@@ -143,9 +137,11 @@ class Thread_1 implements Runnable {
         }
         System.out.println();
 
-        }
     }
-   class Thread_3 implements Runnable {
+}
+
+class Thread_3 implements Runnable {
+
     int tablou[];
     int from;
     int to;
@@ -157,33 +153,36 @@ class Thread_1 implements Runnable {
         this.to = to;
         this.step = step;
     }
+
     @Override
     public void run() {
 
         int p = 1;
-        int c=0;
-        int pp=1;
-        int s=0;
+        int c = 0;
+        int pp = 1;
+        int s = 0;
 
         for (int i = from; i != to; i += step) {
             c++;
             if (c <= 2) {
-                p = p * tablou[i];                }
-            else if(c<=4){
-                pp=pp*tablou[i];
-                s=p+pp;
+                p = p * tablou[i];
+            } else if (c <= 4) {
+                pp = pp * tablou[i];
+                s = p + pp;
             }
-            if(c==4){
-                System.out.println("Produsul este: " + Thread.currentThread().getName() + " " + "primul produs: " + p + " al doilea produs: " + pp + " suma: " + s);                p=1;
-                pp=1;
-                p=1;
-                c=0;
+            if (c == 4) {
+                System.out.println("Produsul este: " + Thread.currentThread().getName() + " " + "primul produs: " + p + " al doilea produs: " + pp + " suma: " + s);
+                p = 1;
+                pp = 1;
+                p = 1;
+                c = 0;
             }
         }
     }
 }
 
-class Thread_4 implements Runnable{
+class Thread_4 implements Runnable {
+
     int tablou[];
     int from;
     int to;
@@ -195,27 +194,29 @@ class Thread_4 implements Runnable{
         this.to = to;
         this.step = step;
     }
+
     @Override
     public void run() {
 
         int p = 1;
-        int c=0;
-        int pp=1;
-        int s=0;
+        int c = 0;
+        int pp = 1;
+        int s = 0;
 
         for (int i = from; i != to; i += step) {
             c++;
             if (c <= 2) {
-                p = p * tablou[i];                }
-            else if(c<=4){
-                pp=pp*tablou[i];
-                s=p+pp;
+                p = p * tablou[i];
+            } else if (c <= 4) {
+                pp = pp * tablou[i];
+                s = p + pp;
             }
-            if(c==4){
-                System.out.println("Produsul este: " + Thread.currentThread().getName() + " " + "primul produs: " + p + " al doilea produs: " + pp + " suma: " + s);                p=1;
-                pp=1;
-                p=1;
-                c=0;
+            if (c == 4) {
+                System.out.println("Produsul este: " + Thread.currentThread().getName() + " " + "primul produs: " + p + " al doilea produs: " + pp + " suma: " + s);
+                p = 1;
+                pp = 1;
+                p = 1;
+                c = 0;
             }
         }
         String numePrenume = "Prepelita Catalin";
@@ -231,4 +232,4 @@ class Thread_4 implements Runnable{
         System.out.println();
     }
 }
-//spatiu
+
