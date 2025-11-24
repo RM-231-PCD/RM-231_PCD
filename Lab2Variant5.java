152030e9f5ca95fb507636fdd75a274b733294a1
Nick22r <132853642+Nick22r@users.noreply.github.com>
2025-11-24 11:48:15 +0200

    Add files via upload
    cod finalizat corecct

--- Lab2Variant5.java
+++ Lab2Variant5.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 class FirUnu extends Thread {
@@ -9,18 +10,12 @@ class FirUnu extends Thread {
 
     @Override
     public void run() {
-        for (int i = 0; i < v.length - 1; i += 2) {
-            int a = v[i];
-            int b = v[i + 1];
-            int produs = a * b;
+        for (int i = 0; i < v.length - 8; i += 8) {
+            int a = v[i] * v[i + 2];
+            int b = v[i + 4] + v[i + 6];
+            int produs = a + b;
             int dif = Math.abs(a - b);
-
-            System.out.println("Unu " + i + " " + (i + 1) + " " + produs + " " + dif + " " + a);
-            try {
-                Thread.sleep(1); 
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            System.out.println(" p1= " + a + " p2= " + b + " suma= " + produs + " dif= " + dif);
         }
     }
 }
@@ -34,31 +29,23 @@ class FirDoi extends Thread {
 
     @Override
     public void run() {
-        for (int i = v.length - 1; i > 0; i -= 2) {
+        for (int i = v.length - 2; i > 0; i -= 2) {
             int a = v[i];
             int b = v[i - 1];
             int produs = a * b;
             int dif = Math.abs(a - b);
-
-            System.out.println("Doi " + i + " " + (i - 1) + " " + produs + " " + dif + " " + b);
-            try {
-                Thread.sleep(1); 
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            System.out.println("Doi " + i + " " + (i - 1) + " produs= " + produs + " dif= " + dif + " b= " + b);
         }
     }
 }
 
 public class Lab2Variant5 {
     public static void main(String[] args) throws InterruptedException {
-
         int[] v = new int[100];
         Random r = new Random();
+        for (int i = 0; i < 100; i++) v[i] = r.nextInt(100);
 
-        for (int i = 0; i < 100; i++) {
-            v[i] = r.nextInt(100);
-        }
+        System.out.println("Vectorul: " + Arrays.toString(v));
 
         FirUnu unu = new FirUnu(v);
         FirDoi doi = new FirDoi(v);
@@ -69,6 +56,17 @@ public class Lab2Variant5 {
         unu.join();
         doi.join();
 
-        System.out.println("cod finalizat");
+        System.out.println("\nBUILD SUCCESSFUL\n");
+
+        String infoStudenti = "Student: Josan Nicolae - RM-231 - Lucrarea de laborator 2";
+        for (char c : infoStudenti.toCharArray()) {
+            System.out.print(c);
+            try {
+                Thread.sleep(100); 
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(); 
     }
 }
