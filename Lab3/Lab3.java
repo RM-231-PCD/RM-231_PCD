9815eda6d6bcc25888e88b19228b23bf47eb4a4e
Danu Procopciuc <procopciucdanu@gmail.com>
2025-12-01 09:58:25 +0200

    modificat 3

--- Lab3/Lab3.java
+++ Lab3/Lab3.java
@@ -129,7 +129,7 @@ class Thread_3 implements Runnable {
             if(i%50==0){
                 System.out.println("");
             }
-            System.out.print(" " + i);
+            System.out.print( Thread.currentThread().getName() +   " " + i+ " ");
             try { Thread.sleep(100); } catch (InterruptedException e) { }
         }
         System.out.println();
@@ -152,7 +152,7 @@ class Thread_4 implements Runnable {
     @Override
     public void run() {
         for (int i = from; i >= to; i--) {
-            System.out.print(" " + i);
+            System.out.print(Thread.currentThread().getName() + " " + i+ " ");
             if(i%50 ==0){
                 System.out.println();
             }
