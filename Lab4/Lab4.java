3c67c3582dba410e374872bd50afce64b4751929
Danu Procopciuc <procopciucdanu@gmail.com>
2025-12-04 10:17:13 +0200

    lucreaza

--- Lab4/Lab4.java
+++ Lab4/Lab4.java
@@ -7,8 +7,7 @@ este îndestulat cu 12 obiecte.
 F - fiecare producător produce câte 2 obiecte de fiecare
 dată
 Produc consoane
-
-    */
+*/
 
 package Lab4;
 
@@ -19,24 +18,26 @@ import java.util.Random;
 public class Lab4 {
     public static void main(String[] args) {
 
-        Depozit depozit = new Depozit(11);
+        Depozit depozit = new Depozit(11, 3);
 
-        Thread p1 = new Thread(new Producator(depozit, 1), "Producator-1");
-        Thread p2 = new Thread(new Producator(depozit, 2), "Producator-2");
-        Thread p3 = new Thread(new Producator(depozit, 3), "Producator-3");
+        Thread p1 = new Thread(new Producator(depozit, 1));
+        Thread p2 = new Thread(new Producator(depozit, 2));
+        Thread p3 = new Thread(new Producator(depozit, 3));
 
-        Thread c1 = new Thread(new Consumator(depozit, 1), "Consumator-1");
-        Thread c2 = new Thread(new Consumator(depozit, 2), "Consumator-2");
+        Thread c1 = new Thread(new Consumator(depozit, 1));
+        Thread c2 = new Thread(new Consumator(depozit, 2));
 
         p1.start();
         p2.start();
         p3.start();
+
         c1.start();
         c2.start();
     }
+
     static class Producator implements Runnable {
-        private Depozit depozit;
-        private int id;
+        private final Depozit depozit;
+        private final int id;
 
         public Producator(Depozit depozit, int id) {
             this.depozit = depozit;
@@ -45,14 +46,17 @@ public class Lab4 {
 
         @Override
         public void run() {
-            for (int i = 0; i < 6; i++) { 
+            for (int i = 0; i < 6; i++) {
                 depozit.produce(id);
             }
+            depozit.anuntaTerminareProductie();
         }
     }
+
     static class Consumator implements Runnable {
-        private Depozit depozit;
-        private int id;
+        private final Depozit depozit;
+        private final int id;
+        private int consumedCount = 0;
 
         public Consumator(Depozit depozit, int id) {
             this.depozit = depozit;
@@ -61,8 +65,13 @@ public class Lab4 {
 
         @Override
         public void run() {
-            for (int i = 0; i < 12; i++) { 
-                depozit.consume(id);
+            for (int i = 0; i < 12; i++) {
+                char item = depozit.consume();
+                if (item == 0) break;
+                consumedCount++;
+                System.out.println("Consumator " + id +
+                        " a consumat: " + item +
+                        " total consumat de el: " + consumedCount);
             }
         }
     }
@@ -71,17 +80,19 @@ public class Lab4 {
         private final int capacity;
         private final Queue<Character> items = new LinkedList<>();
         private final Random rand = new Random();
+        private int producatoriRamas;
 
-        private static final char[] CONSOANE = 
+        private static final char[] CONSOANE =
                 "BCDFGHJKLMNPQRSTVWXYZ".toCharArray();
 
-        public Depozit(int capacity) {
+        public Depozit(int capacity, int nrProducatori) {
             this.capacity = capacity;
+            this.producatoriRamas = nrProducatori;
         }
 
         public synchronized void produce(int producerId) {
             while (items.size() + 2 > capacity) {
-                try { wait(); } catch (InterruptedException e) {}
+                try { wait(); } catch (InterruptedException ignored) {}
             }
 
             char c1 = CONSOANE[rand.nextInt(CONSOANE.length)];
@@ -92,24 +103,30 @@ public class Lab4 {
 
             System.out.println("Producator " + producerId +
                     " a produs: " + c1 + ", " + c2 +
-                    " | total: " + items.size());
+                    " total în depozit: " + items.size());
 
             notifyAll();
         }
 
-        public synchronized void consume(int consumerId) {
-            while (items.isEmpty()) {
-                try { wait(); } catch (InterruptedException e) {}
+        public synchronized char consume() {
+            while (items.isEmpty() && producatoriRamas > 0) {
+                try { wait(); } catch (InterruptedException ignored) {}
+            }
+
+            if (items.isEmpty() && producatoriRamas == 0) {
+                return 0;
             }
 
             char item = items.remove();
-
-            System.out.println("Consumator " + consumerId +
-                    " a consumat: " + item +
-                    " | total: " + items.size());
-
             notifyAll();
+            return item;
+        }
+
+        public synchronized void anuntaTerminareProductie() {
+            producatoriRamas--;
+            if (producatoriRamas == 0) {
+                notifyAll();
+            }
         }
     }
 }
-
